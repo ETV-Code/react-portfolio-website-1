944f69f8c677fa054ab90c5e5f0153e796d2faaa
ETV-Code <emmanuelrt854@outlook.com>
2022-09-01 08:46:06 -0500

    segunda prueba

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Portfolio</h1>
+        <h1>cambio 2</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
